b921b88c5d16e8eaa9f1b42e165e92a912ca8f66
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2021-04-01 10:18:29 +0200

    added and removed tests

---
+++ src/test/java/de/unistuttgart/t2/order/OrderTest.java
@@ -0,0 +1,95 @@
+package de.unistuttgart.t2.order;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import de.unistuttgart.t2.common.domain.saga.CreditCardInfo;
+import de.unistuttgart.t2.common.domain.saga.SagaData;
+import de.unistuttgart.t2.domain.OrderItem;
+import de.unistuttgart.t2.domain.OrderStatus;
+import de.unistuttgart.t2.repository.OrderRepository;
+
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestContext.class)
+public class OrderTest {
+
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	OrderRepository orderRepository;
+	
+	CreditCardInfo card;
+	SagaData data; 
+	
+	String orderid;
+	
+	@BeforeEach
+	public void setup() {
+		orderid = orderRepository.save(new OrderItem("sessionId", OrderStatus.SUCCESS, Date.from(Instant.now()))).getOrderId();
+	}
+	
+	@Test
+	public void testCreateOrder() {
+		// execute	
+		String id = orderService.createOrder("sessionId");
+		
+		//assert
+		assertTrue(orderRepository.existsById(id));
+		assertTrue(orderRepository.findById(id).isPresent());
+		
+		OrderItem item = orderRepository.findById(id).get();
+		assertEquals("sessionId", item.getSessionId());
+		assertEquals(OrderStatus.SUCCESS, item.getStatus());
+	}
+	
+	@Test
+	public void testCreateOrder_failOnNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {	
+			orderService.createOrder(null);
+		});
+	}
+	
+	@Test
+	public void testRejectOrder() {
+		// execute	
+		orderService.rejectOrder(orderid);
+		
+		//assert
+		assertTrue(orderRepository.existsById(orderid));
+		assertTrue(orderRepository.findById(orderid).isPresent());
+		
+		OrderItem item = orderRepository.findById(orderid).get();
+		assertEquals("sessionId", item.getSessionId());
+		assertEquals(OrderStatus.FAILURE, item.getStatus());
+	}
+	
+	@Test
+	public void testRejectOrder_failOnNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			orderService.rejectOrder(null);
+		});
+	}
+	
+	@Test
+	public void testRejectOrder_failWrongOrderId() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			orderService.rejectOrder("id_that_does_not_match_any_order");
+		});
+	}
+	
+	
+}
---
+++ src/test/java/de/unistuttgart/t2/order/TestContext.java
@@ -0,0 +1,19 @@
+package de.unistuttgart.t2.order;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import de.unistuttgart.t2.repository.OrderRepository;
+
+@Configuration
+@EnableMongoRepositories(basePackageClasses = {OrderRepository.class})
+public class TestContext {
+	
+	@Bean
+	public OrderService service() {
+		return new OrderService();
+	}
+
+}
