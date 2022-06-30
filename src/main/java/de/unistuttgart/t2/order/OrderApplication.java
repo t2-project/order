d98ab6fb53ed7fa638087f80675b4ac966ee07b9
delvh <leon@kske.dev>
2022-06-30 19:37:04 +0200

    Add autoscaling management to order

--- src/main/java/de/unistuttgart/t2/order/OrderApplication.java
+++ src/main/java/de/unistuttgart/t2/order/OrderApplication.java
@@ -1,30 +1,28 @@
 package de.unistuttgart.t2.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import de.unistuttgart.t2.common.BaseScan;
 import de.unistuttgart.t2.order.repository.OrderRepository;
 import de.unistuttgart.t2.order.saga.OrderCommandHandler;
-import io.eventuate.tram.sagas.participant.SagaCommandDispatcher;
-import io.eventuate.tram.sagas.participant.SagaCommandDispatcherFactory;
+import io.eventuate.tram.sagas.participant.*;
 import io.eventuate.tram.sagas.spring.participant.SagaParticipantConfiguration;
 import io.eventuate.tram.spring.consumer.kafka.EventuateTramKafkaMessageConsumerConfiguration;
 import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import io.eventuate.tram.spring.optimisticlocking.OptimisticLockingDecoratorConfiguration;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
 
-@Import({ SagaParticipantConfiguration.class, TramMessageProducerJdbcConfiguration.class,
-        EventuateTramKafkaMessageConsumerConfiguration.class, OptimisticLockingDecoratorConfiguration.class })
+@Import({	SagaParticipantConfiguration.class, TramMessageProducerJdbcConfiguration.class,
+			EventuateTramKafkaMessageConsumerConfiguration.class,
+			OptimisticLockingDecoratorConfiguration.class })
 @EnableJpaRepositories
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = BaseScan.class)
 @EnableMongoRepositories(basePackageClasses = OrderRepository.class)
 public class OrderApplication {
 
