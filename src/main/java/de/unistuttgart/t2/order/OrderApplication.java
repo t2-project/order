474d9a272129015e2f90007118cad4833a355d49
delvh <leon@kske.dev>
2022-08-19 15:48:52 +0200

    Remove redundant class scan and annotation

--- src/main/java/de/unistuttgart/t2/order/OrderApplication.java
+++ src/main/java/de/unistuttgart/t2/order/OrderApplication.java
@@ -1,12 +1,11 @@
 package de.unistuttgart.t2.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import de.unistuttgart.t2.common.BaseScan;
 import de.unistuttgart.t2.order.repository.OrderRepository;
 import de.unistuttgart.t2.order.saga.OrderCommandHandler;
 import io.eventuate.tram.sagas.participant.*;
@@ -21,8 +20,7 @@ import io.swagger.v3.oas.models.info.Info;
     EventuateTramKafkaMessageConsumerConfiguration.class,
     OptimisticLockingDecoratorConfiguration.class })
 @EnableJpaRepositories
-@EnableAutoConfiguration
-@SpringBootApplication(scanBasePackageClasses = { BaseScan.class, OrderApplication.class })
+@SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = OrderRepository.class)
 public class OrderApplication {
 
